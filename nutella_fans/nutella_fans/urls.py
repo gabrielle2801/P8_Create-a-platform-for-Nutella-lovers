c8bde9d97f77a0514c9719393e69d38d450684a6
Gabrielle Azadian <gabrielleazadian@gmail.com>
2021-07-06 14:45:14 +0200

    Update files urls

--- nutella_fans/nutella_fans/urls.py
+++ nutella_fans/nutella_fans/urls.py
@@ -25,6 +25,4 @@ urlpatterns = [
     path('', include('users_account.urls')),
     path('', TemplateView.as_view(template_name='base.html'), name='base'),
     path('account/', include('django.contrib.auth.urls')),
-    path('', include('product.urls'))
-
 ]
